14f7ed9312120c528f83d2569a83f9225fb2fd34
alkanik <alkanik@gmail.com>
2022-09-14 15:21:54 +0300

    added mailsender

--- EmailSender/Program.cs
+++ EmailSender/Program.cs
@@ -16,15 +16,20 @@ IHost host = Host.CreateDefaultBuilder(args)
         logging.SetMinimumLevel(LogLevel.Trace);
         logging.AddNLog(hostContext.Configuration, new NLogProviderOptions() { LoggingConfigurationSectionName = "NLog" });
     })
-    .ConfigureServices(services =>
+    .ConfigureAppConfiguration((hostContext, configBuilder) =>
     {
+        configBuilder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .Build();
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        IConfiguration configuration = hostContext.Configuration;
+        services.Configure<EmailConfiguration>(configuration.GetSection("EmailConfiguration"));
         services.AddHostedService<Worker>();
         services.AddScoped<IMailSender, MailSender>();
-        //var emailConfig = Configuration
-        //.GetSection("EmailConfiguration")
-        //.Get<EmailConfiguration>();
-        //builder.Services.AddSingleton(emailConfig);
-
     })
     .Build();
 
--- EmailSender/Worker.cs
+++ EmailSender/Worker.cs
@@ -1,14 +1,19 @@
+using EmailSender.Infrastructure;
+using System.Configuration;
+
 namespace EmailSender;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IServiceProvider _serviceProvider;
+    private readonly IConfiguration _configuration;
 
-    public Worker(ILogger<Worker> logger, IServiceProvider serviceProvider)
+    public Worker(ILogger<Worker> logger, IServiceProvider serviceProvider, IConfiguration configuration)
     {
         _logger = logger;
         _serviceProvider = serviceProvider;
+        _configuration = configuration;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -16,6 +21,10 @@ public class Worker : BackgroundService
         while (!stoppingToken.IsCancellationRequested)
         {
             _logger.LogInformation("EmailSender running at: {time}", DateTimeOffset.Now);
+            var emailConfig = _configuration
+                .GetSection("EmailConfiguration")
+                .Get<EmailConfiguration>();
+
             using (var scope = _serviceProvider.CreateScope())
             {
                 IMailSender mailsender =
@@ -23,6 +32,7 @@ public class Worker : BackgroundService
                 var message = new Message(new string[] { "IncredibleEmailSender@gmail.com" }, "Hi", "All you need is mail!");
                 mailsender.SendEmail(message);
             }
+
 
             await Task.Delay(5000, stoppingToken);
         }

84ab711218028710b1f5840960516167f503c0dd
alkanik <alkanik@gmail.com>
2022-09-14 10:59:16 +0300

    added NLog

--- EmailSender/Program.cs
+++ EmailSender/Program.cs
@@ -1,6 +1,17 @@
 using EmailSender;
+using NLog.Extensions.Logging;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
+    {
+        options.ServiceName = "EmailSender";
+    })
+    .ConfigureLogging((hostContext, logging) =>
+    {
+        logging.ClearProviders();
+        logging.SetMinimumLevel(LogLevel.Trace);
+        logging.AddNLog(hostContext.Configuration, new NLogProviderOptions() { LoggingConfigurationSectionName = "NLog" });
+    })
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();

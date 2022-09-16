5f4eb3c6d83b3b39996bb3f00948d21f12c5066a
alkanik <alkanik@gmail.com>
2022-09-16 20:47:16 +0300

    deleted test sendler

--- EmailSender/Worker.cs
+++ EmailSender/Worker.cs
@@ -20,23 +20,8 @@ public class Worker : BackgroundService
     {
         _logger.LogInformation("EmailSender running at: {time}", DateTimeOffset.Now);
 
-        while (!stoppingToken.IsCancellationRequested)
-        { 
-            var emailConfig = _configuration
+        var emailConfig = _configuration
                 .GetSection("EmailConfiguration")
                 .Get<EmailConfiguration>();
-
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                IMailSender mailsender =
-                    scope.ServiceProvider.GetRequiredService<IMailSender>();
-                var message = new Message(new string[] { "IncredibleEmailSender@gmail.com" }, "Hi", "All you need is mail!");
-                mailsender.SendEmail(message);
-                _logger.LogInformation("EmailSender send a mail at: {time}", DateTimeOffset.Now);
-            }
-
-
-            await Task.Delay(5000, stoppingToken);
-        }
     }
 }
